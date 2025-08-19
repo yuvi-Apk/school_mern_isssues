73ac1681e86e55d2884a5b0b96193bbe58ee5095
YUBARAJ MAHANTA <yubrajmahanta@gmail.com>
2025-08-19 14:04:43 +0530

    Update db.js

--- Backend/src/Config/db.js
+++ Backend/src/Config/db.js
@@ -3,18 +3,28 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "school_db",
+//   port: 3306, // Use the default MySQL port
+//   // ssl: {
+//   //   rejectUnauthorized: true,
+//   // },
+//   // waitForConnections: true,
+//   // connectionLimit: 10,
+//   // queueLimit: 0,
+// });
+
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "school_db",
-  port: 3306, // Use the default MySQL port
-  // ssl: {
-  //   rejectUnauthorized: true,
-  // },
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // queueLimit: 0,
+  host: "103.185.75.157",
+  user: "imslite_local_test",
+  password: 'Mc$zdCf4J%wM',
+  database: "imslite_school_db",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 // Test connection
