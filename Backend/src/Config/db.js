561c70d9399ea6ca6b88cbf4921e8aec46fd7842
YUBARAJ MAHANTA <yubrajmahanta@gmail.com>
2025-08-19 14:07:44 +0530

    Update db.js
    .env removed

--- Backend/src/Config/db.js
+++ Backend/src/Config/db.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
 // const pool = mysql.createPool({
 //   host: "localhost",
